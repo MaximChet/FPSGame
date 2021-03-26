95791aca7aa437cb6bf186d9ccfe40f99ff8f431
Максим Четыркин <skarpion234@gmai.com>
2021-03-26 11:39:33 +0300

    FPS game

---
+++ Assets/Rotate.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour
+{
+    public int rotateX;
+    public int rotateY;
+    public int rotateZ;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rotateX, rotateY, rotateZ);
+    }
+}
---
+++ Assets/Shoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public GameObject arrowPrefab;
+    public Transform arrowLocaiton;
+    public float shotPower;
+    public AudioSource shootSound;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            Instantiate(arrowPrefab, arrowLocaiton.position, arrowLocaiton.rotation).GetComponent<Rigidbody>().AddRelativeForce(Vector3.forward * shotPower);
+            shootSound.Play();
+        }
+    }
+
+}
